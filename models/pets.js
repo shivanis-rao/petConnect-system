e41ac848e1d096de11b8d31b8249357f3abcb4fa
debo <debjyoti.barman@nineleaps.com>
2026-03-10 15:32:53 +0530

    pet addded

---
+++ models/pets.js
@@ -0,0 +1,59 @@
+import { Model, DataTypes } from "sequelize";
+
+export default (sequelize) => {
+  class Pet extends Model {
+    static associate(models) {
+      // example future relation
+      // Pet.belongsTo(models.User, { foreignKey: "shelter_id" });
+    }
+  }
+
+  Pet.init(
+    {
+      name: DataTypes.STRING,
+      species: DataTypes.STRING,
+      breed: DataTypes.STRING,
+      age: DataTypes.INTEGER,
+      gender: DataTypes.STRING,
+
+      vaccinated: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+
+      sterilized: DataTypes.STRING,
+
+      special_needs: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+
+      health_status: DataTypes.TEXT,
+      temperament: DataTypes.TEXT,
+      rescue_story: DataTypes.TEXT,
+
+      adoption_fee: DataTypes.INTEGER,
+
+      status: DataTypes.STRING,
+
+      good_with_kids: DataTypes.BOOLEAN,
+
+      shelter_id: DataTypes.INTEGER,
+
+      listed_at: DataTypes.DATE,
+      adopted_at: DataTypes.DATE,
+      deleted_at: DataTypes.DATE,
+
+      created_by: DataTypes.INTEGER,
+      updated_by: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      modelName: "Pet",
+      tableName: "Pets",
+      timestamps: true,
+    }
+  );
+
+  return Pet;
+};
