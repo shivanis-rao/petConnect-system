4f9c363b257b61b899387729f3247cdef30b8b29
nineleaps <spandana.g@gmail.com>
2026-03-10 13:52:15 +0530

    Changes in migration file

--- migrations/20260310061729-create-user.js
+++ migrations/20260310061729-create-user.js
@@ -1,6 +1,7 @@
 'use strict';
+
 /** @type {import('sequelize-cli').Migration} */
-module.exports = {
+export default {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Users', {
       id: {
@@ -25,6 +26,7 @@ module.exports = {
       }
     });
   },
+
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('Users');
   }
